bd8f14f7280f132165a9cfb838a9d5eb1d90252d
tungcn <austin.ngyuen@yahoo.com>
2020-06-11 21:27:17 -0700

    BuildsMicrocenterURL

--- PC-Pair/microcenter_search.py
+++ PC-Pair/microcenter_search.py
@@ -2,7 +2,16 @@
 This file contains a function that will perform a search on microcenter and 
 find the price of an item at its product page.
 '''
-
-print("dog")
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup
+def microcentersearch(searchstr:str):
+    searchstr = searchstr.replace(' ','+') #replaces any spaces with + signs because URL uses + as spaces
+    print('searchstr=', searchstr) #tests searchstr
+    microcenter_url = "https://www.microcenter.com/search/search_results.aspx?N=&cat=&Ntt=" + searchstr + "&searchButton=search" #builds URl to grab from
+    print("microcenter_url=",microcenter_url) #tests microcenterURL
+    
+microcentersearch('RTX 2080')
+microcentersearch('RTX2080')
+microcentersearch('RTX  2080')
 
 

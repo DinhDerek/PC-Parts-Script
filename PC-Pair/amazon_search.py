bb33483adb56da2ed72b31fd224bea1515c89b2e
Derek Dinh <DinhDerek@yahoo.com>
2020-06-11 18:39:05 -0700

    Added the two .py files which will contain the search functions.

---
+++ PC-Pair/amazon_search.py
@@ -0,0 +1,6 @@
+'''
+This file contains a function that will perform a search on amazon and 
+find the price of an item at its product page.
+'''
+
+print("Test")
---
+++ PC-Pair/microcenter_search.py
@@ -0,0 +1,7 @@
+'''
+This file contains a function that will perform a search on microcenter and 
+find the price of an item at its product page.
+'''
+
+print("Test")
+

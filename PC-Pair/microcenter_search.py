054c0ec2a3f2a2f1a92917519319a614843ed2ba
tungcn <austin.ngyuen@yahoo.com>
2020-06-11 22:43:07 -0700

    microcenter_search_draft
    Done:
    -Grabs prices,name,and brand of input str
    Not Done:
    - Grabs other GPUs ex. 'RTX 2080' grabs RTX 2080 super and RTX 2080 Ti
    (Fix by making it exactly only RTX 2080 or maybe use price as a limiter)
    -Only accesses evga,asus,msi brands only

--- PC-Pair/microcenter_search.py
+++ PC-Pair/microcenter_search.py
@@ -3,15 +3,30 @@ This file contains a function that will perform a search on microcenter and
 find the price of an item at its product page.
 '''
 from urllib.request import urlopen as uReq
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as soup
 def microcentersearch(searchstr:str):
     searchstr = searchstr.replace(' ','+') #replaces any spaces with + signs because URL uses + as spaces
-    print('searchstr=', searchstr) #tests searchstr
+    for brand in ['evga','asus','msi']: #grabs brand name from str
+        if brand in searchstr.lower():
+            brand_name = brand
     microcenter_url = "https://www.microcenter.com/search/search_results.aspx?N=&cat=&Ntt=" + searchstr + "&searchButton=search" #builds URl to grab from
-    print("microcenter_url=",microcenter_url) #tests microcenterURL
+    # Accesses HTML code
+    URLRequest = uReq(microcenter_url)
+    page_html = URLRequest.read()
+    URLRequest.close()
+    page_soup = soup(page_html, "html.parser")
+    #
+    container = page_soup.findAll("div",{"class":"pDescription compressedNormal2"}) #contains HTML info of computer part
+    #loops through items in the HTML accessing their data
+    for item in container:
+        product_brand = item.a['data-brand']
+        if brand_name.lower() == product_brand.lower():
+            product_name =item.a['data-name']
+            product_price = item.a['data-price']
+            print(' Product Brand:',product_brand, "\n",'Product Name:', product_name,"\n",'Product Price:', product_price,"\n")
+   
     
-microcentersearch('RTX 2080')
-microcentersearch('RTX2080')
-microcentersearch('RTX  2080')
-
+microcentersearch('EVGA RTX 2080')
+#microcentersearch('msi RTX 2080')
+#microcentersearch('AsUS RTX  2080')
 

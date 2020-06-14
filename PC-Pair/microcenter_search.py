fd5aba14d77b92e4e1522fcb114589b6a18918ed
tungcn <austin.ngyuen@yahoo.com>
2020-06-13 18:31:43 -0700

    Microcenter_Search 1.1 Completed
    Implemented support for OOS items;
    - OOS items will return (-1,"")

--- PC-Pair/microcenter_search.py
+++ PC-Pair/microcenter_search.py
@@ -16,11 +16,14 @@ def microcenter_search(searchstr:str) -> (float,str):
     URLRequest.close()
     page_soup = soup(page_html, "html.parser")
     #
-    container = page_soup.findAll("div",{"class": re.compile('^pDescription compressedNormal(\d)*$')}) #contains HTML info of computer part, repattern because microcenter uses different classes on searches but with diff numbering only #POSSIBLE ERROR: NOT ALL PRODUCTS USE THE PATTERN BUT WE ARE ASSUMING
-    product_price = container[0].a['data-price']
-    product_link = container[0].a['href']
-    return (float(product_price.strip(',')),'https://www.microcenter.com' + str(product_link)) #returns a tuple of (price,url) types = (float,str)
+    parent_class = page_soup.find('li',{"id": 'pwrapper_0'})
+    product_info = parent_class.find("div",{"class": re.compile('^pDescription compressedNormal(\d)*$')}) #contains HTML info of computer part, repattern because microcenter uses different classes on searches but with diff numbering only #POSSIBLE ERROR: NOT ALL PRODUCTS USE THE PATTERN BUT WE ARE ASSUMING
+    stock = parent_class.find("div",{"class":'stock'})
+    if stock.text.replace("\n","") == "Sold Out": #if sold out
+        return (-1.0,"")
+    else:
+        product_price = product_info.a['data-price']
+        product_link = product_info.a['href']
+        return (float(product_price.replace(',' ,"")),'https://www.microcenter.com' + str(product_link)) #returns a tuple of (price,url) types = (float,str)
     
-
-
 
